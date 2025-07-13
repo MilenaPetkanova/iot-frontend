94d8f283a2d8357c8db5502b2319f9095fcf77bb
Milena Petkanova <milenapetkanova@gmail.com>
2025-07-13 11:23:35 +0300

    config tslint and prettify

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js'
+import vue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+import prettier from 'eslint-plugin-prettier'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsparser from '@typescript-eslint/parser'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsparser,
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      vue,
+      '@typescript-eslint': tseslint,
+      prettier
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'prettier/prettier': ['warn', {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'none',
+        endOfLine: 'auto'
+      }]
+    }
+  }
+]
